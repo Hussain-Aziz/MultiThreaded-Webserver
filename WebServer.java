5a5bfe313620dfc9978972166ae1893ff4bcd978
Hussain Aziz <hussain.azizsaif@gmail.com>
2023-04-04 19:46:02 -0400

    read the headers from the client

--- WebServer.java
+++ WebServer.java
@@ -53,6 +53,27 @@ final class HttpRequest implements Runnable
     private void processRequest() throws Exception
     {
         // Get a reference to the socket's input and output streams.
+        InputStream is = socket.getInputStream();
+        DataOutputStream os = new DataOutputStream(socket.getOutputStream());
+        // Set up input stream filters.
+        InputStreamReader isr = new InputStreamReader(is);
+        BufferedReader br = new BufferedReader(isr);
 
+        // Get the request line of the HTTP request message.
+        String requestLine = br.readLine();
+        // Display the request line.
+        System.out.println();
+        System.out.println(requestLine);
+
+        // Get and display the header lines.
+        String headerLine = null;
+        while ((headerLine = br.readLine()).length() != 0) {
+        System.out.println(headerLine);
+        }
+
+        // Close streams and socket.
+        os.close();
+        br.close();
+        socket.close();
     }
 }
